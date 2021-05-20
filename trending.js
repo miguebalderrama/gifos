ae47de27f44c0a244085b871d425c1fcd0f14bb5
Miguel Balderrama <miguel.balderr@gmail.com>
2021-05-19 23:45:10 -0300

    favoriteamos en todas las acrs del trending y mantiene estado

--- trending.js
+++ trending.js
@@ -11,8 +11,8 @@ function toggleMenu(event) {
 // event
 menu.addEventListener("click", toggleMenu, false);
 var favtrend0=0;
-var favtrend0=1;
-var favtrend0=2;
+var favtrend1=0;
+var favtrend2=0;
 // addEventListener version
 // trending
 let apiKey = "bw24LFlb3BXkhx9uB9goI91bEaW3Sm8H";
@@ -57,7 +57,7 @@ fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=5`)
       });
       count=0;
 
-    for (var a = 0; a < 3; a++) {
+    for (let a = 0; a < 3; a++) {
       document.getElementById("img" + a).src = arraygifs[a];
       document.getElementById("user" + a).textContent = arrayUser[a];
       document.getElementById("tit" + a).textContent = arrayTitle[a];
@@ -99,6 +99,24 @@ forward.addEventListener("click", () => {
 var reward = document.getElementById("left");
 reward.addEventListener("click", () => {
   counti--;
+  if(favtrend0===1){
+    let d = document.getElementById("favtrend0");
+   let d_nested = document.getElementById("imgfav0");
+    d.removeChild(d_nested);
+    favtrend0=0;
+    } 
+    if(favtrend1===1){
+      let d = document.getElementById("favtrend1");
+     let d_nested = document.getElementById("imgfav1");
+      d.removeChild(d_nested);
+      favtrend1=0;
+      }  
+      if(favtrend2===1){
+        let d = document.getElementById("favtrend2");
+       let d_nested = document.getElementById("imgfav2");
+        d.removeChild(d_nested);
+        favtrend2=0;
+        }
   atras();
 });
 
@@ -126,6 +144,28 @@ function atras() {
     }
   }
   console.log(counti);
+  for (let a = 0; a < 3; a++) {
+    if(arrayLike.find(element => element == (document.getElementById("favtrend"+a).name))){
+      if(a==0){
+    console.log("hay un gustado en card 0")
+    favtrend0=1;
+  }
+  if(a==1){
+    console.log("hay un gustado en card 1")
+    favtrend1=1;
+  }
+  if(a==2){
+    console.log("hay un gustado en card 2")
+    favtrend2=1;
+  }
+    let imgfavs = document.createElement("img");
+      imgfavs.src = "..//assets/icon-fav-active.svg";
+      imgfavs.setAttribute("width", "18px");
+      imgfavs.className = "imgfavs";
+      imgfavs.id="imgfav"+a;
+      document.getElementById("favtrend"+a).appendChild(imgfavs);
+    }
+  }
 }
 
 function adelante() {
@@ -153,16 +193,28 @@ function adelante() {
     }
   }
   console.log(counti);
-  if(arrayLike.find(element => element == (document.getElementById("favtrend0").name))){
+  for (let a = 0; a < 3; a++) {
+  if(arrayLike.find(element => element == (document.getElementById("favtrend"+a).name))){
+    if(a==0){
   console.log("hay un gustado en card 0")
   favtrend0=1;
+}
+if(a==1){
+  console.log("hay un gustado en card 1")
+  favtrend1=1;
+}
+if(a==2){
+  console.log("hay un gustado en card 2")
+  favtrend2=1;
+}
   let imgfavs = document.createElement("img");
     imgfavs.src = "..//assets/icon-fav-active.svg";
     imgfavs.setAttribute("width", "18px");
     imgfavs.className = "imgfavs";
-    imgfavs.id="imgfav0";
-    document.getElementById("favtrend0").appendChild(imgfavs);
+    imgfavs.id="imgfav"+a;
+    document.getElementById("favtrend"+a).appendChild(imgfavs);
   }
+}
 }
 ///////////// listener de los icinos de los trending/////////////////////////
 
@@ -206,8 +258,9 @@ amptrendcero.addEventListener("click", function (e){
   document.getElementById("modal").style = "display:block";
   document.getElementById("imagen_ampliada").src = urlmodal;
 })
+/////////////////////////favoritos detectcion/////////////////////
 
-var favtrend=document.getElementById("favtrend0")
+ favtrend=document.getElementById("favtrend0")
 favtrend.addEventListener("click", function (e){
   e.preventDefault();
   favtrend0=1;
@@ -225,7 +278,41 @@ favtrend.addEventListener("click", function (e){
     localStorage.setItem("favoritosLocal", JSON.stringify(gustados));
 })
 
+ favtrend1=document.getElementById("favtrend1")
+favtrend1.addEventListener("click", function (e){
+  e.preventDefault();
+  favtrend1=1;
+  console.log("detectamos el segundo fav"  )
+  let favoritrend1 = document.getElementById("favtrend1").name;
+  arrayLike.push(favoritrend1);
+  let imgfav = document.createElement("img");
+    imgfav.src = "..//assets/icon-fav-active.svg";
+    imgfav.setAttribute("width", "18px");
+    imgfav.className = "imgfavs";
+    imgfav.id="imgfav1";
+    document.getElementById("favtrend1").appendChild(imgfav);
+    console.log(favoritrend1);
+    gustados.push(favoritrend1);
+    localStorage.setItem("favoritosLocal", JSON.stringify(gustados));
+})
 
+favtrend2=document.getElementById("favtrend2")
+favtrend2.addEventListener("click", function (e){
+  e.preventDefault();
+  favtrend2=1;
+  console.log("detectamos el tercer fav"  )
+   favoritrend2 = document.getElementById("favtrend2").name;
+  arrayLike.push(favoritrend2);
+  let imgfav = document.createElement("img");
+    imgfav.src = "..//assets/icon-fav-active.svg";
+    imgfav.setAttribute("width", "18px");
+    imgfav.className = "imgfavs";
+    imgfav.id="imgfav2";
+    document.getElementById("favtrend2").appendChild(imgfav);
+    console.log(favoritrend2);
+    gustados.push(favoritrend2);
+    localStorage.setItem("favoritosLocal", JSON.stringify(gustados));
+})
 
 function downloadTrend(card){
   
