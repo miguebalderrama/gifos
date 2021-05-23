070a236aa7e14cb43f6eb271cb78660b44e6ba85
Miguel Balderrama <miguel.balderr@gmail.com>
2021-05-23 20:48:07 -0300

    cambiamos nombres de button slider

--- fav.js
+++ fav.js
@@ -6,24 +6,5 @@ document.getElementById("cerrar_modal").addEventListener("click", function (e) {
     
   });
 
-  fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        for (let index = 0; index < indice; index++) {
-          var d = document.getElementById("busqueda");
-          var d_nested = document.getElementById("sug" + index);
-          d.removeChild(d_nested);
-        }
-        indice = 0;
-        json.data.forEach((element) => {
-          console.log(element.name);
-          let div = document.createElement("div");
-          div.innerText = element.name;
-          div.setAttribute("class", "sug");
-          div.setAttribute("id", "sug" + indice);
-          div.style=`background: url("..//assets/icon-search.svg") left/4% no-repeat`;
-          document.getElementById("busqueda").appendChild(div);
-          indice++;
-        });
-      })
-      .catch((error) => (document.body.appendChild = error));
+
+  
