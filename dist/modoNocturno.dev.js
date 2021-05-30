5de2be9a9cfafaeed136163056956cdbb09b541a
Miguel Balderrama <miguel.balderr@gmail.com>
2021-05-30 01:53:22 -0300

    avanzamos modo nocturno, trabajamos con los backgrounds

--- dist/modoNocturno.dev.js
+++ dist/modoNocturno.dev.js
@@ -2,6 +2,7 @@
 
 // *   MODO NOCTURNO  *  \º
 var $switchThemeBtn = document.querySelector('#switchTheme');
+var $logo = document.querySelector('#logo');
 
 var switchTheme = function switchTheme() {
   document.body.classList.toggle('darkMode'); // *   GUARDAR EN LOCALSTORAGE   *  \\
@@ -11,29 +12,31 @@ var switchTheme = function switchTheme() {
   } else {
     localStorage.setItem('dark-mode', false);
   }
+
+  setLocalStorageTheme();
 };
 
 $switchThemeBtn.addEventListener('click', switchTheme); // *   CAMBIO DE  IMAGENES, SEGUN MODO NOC O DIURNO  *   \\
 
 var setLocalStorageTheme = function setLocalStorageTheme() {
+  console.log("Me ejecute");
+
   if (localStorage.getItem('dark-mode') == 'true') {
     document.body.classList.add('darkMode');
     $switchThemeBtn.textContent = 'Modo Diurno';
-    $logo.src = 'assets/logo-mobile-modo-noct.svg';
-    $crearGifBtn.src = 'assets/CTA-crar-gifo-modo-noc.svg';
-    $burgerMenu.src = 'assets/burger-modo-noct.svg';
-    $navbarSearchBtn.src = 'assets/icon-search-mod-noc.svg';
-    $navbarSearchCloseBtn.src = 'assets/close-modo-noct.svg';
-    $searchBtn.src = 'assets/icon-search-mod-noc.svg';
-    $searchCloseBtn.src = 'assets/close-modo-noct.svg';
-    $previousBtn.src = 'assets/button-slider-left-md-noct.svg';
-    $nextBtn.src = 'assets/button-slider-right-md-noct.svg';
-    $camera.src = 'assets/camara-modo-noc.svg';
-    $celuloide.src = 'assets/pelicula-modo-noc.svg';
+    $logo.src = 'assets/Logo-modo-noc.svg';
+    $crearGifBtn.src = 'assets/CTA-crar-gifo-modo-noc.svg'; //	$burgerMenu.src = 'assets/burger-modo-noct.svg';
+    //	$navbarSearchBtn.src = 'assets/icon-search-mod-noc.svg';
+    //	$navbarSearchCloseBtn.src = 'assets/close-modo-noct.svg';
+    //	$searchBtn.src = 'assets/icon-search-mod-noc.svg';
+    //	$searchCloseBtn.src = 'assets/close-modo-noct.svg';
+    //	$previousBtn.src = 'assets/button-slider-left-md-noct.svg';
+    //	$nextBtn.src = 'assets/button-slider-right-md-noct.svg';
+    //	$camera.src = 'assets/camara-modo-noc.svg';
+    //	$celuloide.src = 'assets/pelicula-modo-noc.svg';
   } else {
     document.body.classList.remove('darkMode');
+    $logo.src = 'img/logo-mobile.svg';
     $switchThemeBtn.textContent = 'Modo Nocturno';
   }
-};
-
-setLocalStorageTheme();
+}; //setLocalStorageTheme();
--- modoNocturno.js
+++ modoNocturno.js
@@ -1,5 +1,7 @@
 // *   MODO NOCTURNO  *  \º
 const $switchThemeBtn = document.querySelector('#switchTheme');
+const $logo = document.querySelector('#logo');
+
 const switchTheme = () => {
 	document.body.classList.toggle('darkMode');
 
@@ -9,6 +11,7 @@ const switchTheme = () => {
 	} else {
 		localStorage.setItem('dark-mode', false);
 	}
+	setLocalStorageTheme();
 };
 
 $switchThemeBtn.addEventListener('click', switchTheme);
@@ -19,8 +22,9 @@ const setLocalStorageTheme = () => {
 	if (localStorage.getItem('dark-mode') == 'true') {
 		document.body.classList.add('darkMode');
 		$switchThemeBtn.textContent = 'Modo Diurno';
-	//  	$logo.src = 'assets/logo-mobile-modo-noct.svg';
-	//	$crearGifBtn.src = 'assets/CTA-crar-gifo-modo-noc.svg';
+	$logo.src = 'assets/Logo-modo-noc.svg';
+	//$crearGifBtn.src = 'assets/CTA-crar-gifo-modo-noc.svg';
+	document.documentElement.style.setProperty('--nombreVariable', 'url("assets/CTA-crear-gifo-hover-modo-noc.svg")');
 	//	$burgerMenu.src = 'assets/burger-modo-noct.svg';
 	//	$navbarSearchBtn.src = 'assets/icon-search-mod-noc.svg';
 	//	$navbarSearchCloseBtn.src = 'assets/close-modo-noct.svg';
@@ -32,9 +36,11 @@ const setLocalStorageTheme = () => {
 	//	$celuloide.src = 'assets/pelicula-modo-noc.svg';
 	} else {
 		document.body.classList.remove('darkMode');
+		$logo.src ='img/logo-mobile.svg';
 		$switchThemeBtn.textContent = 'Modo Nocturno';
+		document.documentElement.style.setProperty('--nombreVariable', 'url("assets/button-crear-gifo.svg")');
 	}
 	
 };
 
-setLocalStorageTheme();
+//setLocalStorageTheme();
