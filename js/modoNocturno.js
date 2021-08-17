868bdc804b0e391090c7df600901f33f517b0643
Miguel Balderrama <miguel.balderr@gmail.com>
2021-08-17 15:14:24 -0300

    organizando css index

--- js/modoNocturno.js
+++ js/modoNocturno.js
@@ -23,30 +23,30 @@ const setLocalStorageTheme = () => {
 		document.body.classList.add('darkMode');
 		$switchThemeBtn.textContent = 'Modo Diurno';
 	$logo.src = 'assets/Logo-modo-noc.svg';	
-	document.documentElement.style.setProperty('--nombreVariable', 'url("assets/CTA-crear-gifo-hover-modo-noc.svg")');
-	document.documentElement.style.setProperty('--lupa', 'url("assets/icon-search-mod-noc.svg")');
+	document.documentElement.style.setProperty('--nombreVariable', 'url("../assets/CTA-crear-gifo-hover-modo-noc.svg")');
+	document.documentElement.style.setProperty('--lupa', 'url("../assets/icon-search-mod-noc.svg")');
 	document.documentElement.style.setProperty('--colorPrimario', '#ffffff');
-	document.documentElement.style.setProperty('--close', 'url("assets/close-modo-noct.svg")');
+	document.documentElement.style.setProperty('--close', 'url("../assets/close-modo-noct.svg")');
 	document.documentElement.style.setProperty('--verMas', '#37383C');
 	document.documentElement.style.setProperty('--verMasHover', '#ffffff');
 	document.documentElement.style.setProperty('--verMasFuenteHover', '#000000');
-	document.documentElement.style.setProperty('--crearGifoHover', 'url("assets/CTA-crear-gifo-hover-modo-noc.svg")');
+	document.documentElement.style.setProperty('--crearGifoHover', 'url("../assets/CTA-crear-gifo-hover-modo-noc.svg")');
 	document.documentElement.style.setProperty('--textModal', '#ffffff');	
 	
 	} else {
 		document.body.classList.remove('darkMode');
 		$logo.src ='img/logo-mobile.svg';	
 		$switchThemeBtn.textContent = 'Modo Nocturno';
-		document.documentElement.style.setProperty('--nombreVariable', 'url("assets/button-crear-gifo.svg")');
-		document.documentElement.style.setProperty('--lupa', 'url("assets/icon-search.svg")');
-		document.documentElement.style.setProperty('--close', 'url("assets/close.svg")');
+		document.documentElement.style.setProperty('--nombreVariable', 'url("../assets/button-crear-gifo.svg")');
+		document.documentElement.style.setProperty('--lupa', 'url("../assets/icon-search.svg")');
+		document.documentElement.style.setProperty('--close', 'url("../assets/close.svg")');
 		document.documentElement.style.setProperty('--colorPrimario', '#572ee5');
 		document.documentElement.style.setProperty('--verMasHover', '#572ee5');
 		document.documentElement.style.setProperty('--colorPrimario', '#572ee5');
 		document.documentElement.style.setProperty('--verMas', '#ffffff');
 		document.documentElement.style.setProperty('--ColorVerMas', '#572ee5');
 		document.documentElement.style.setProperty('--verMasFuenteHover', '#ffffff');
-		document.documentElement.style.setProperty('--crearGifoHover', 'url("assets/CTA-crear-gifo-hover.svg")');
+		document.documentElement.style.setProperty('--crearGifoHover', 'url("../assets/CTA-crear-gifo-hover.svg")');
 		document.documentElement.style.setProperty('--textModal', '#37383c');
 		
 	}
